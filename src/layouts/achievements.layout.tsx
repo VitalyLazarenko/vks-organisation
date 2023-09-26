8ca1455bbe9c99b7cbe4062d547dadb69e1c139f
VitalyLazarenko <vlazarenko997@gmail.com>
2023-09-26 23:32:27 +0300

    VKS: WHY CHOOSE US: we will need to change icons. OUR ACHIEVEMENTS: need add BG.

--- src/layouts/achievements.layout.tsx
+++ src/layouts/achievements.layout.tsx
@@ -4,8 +4,40 @@ export const AchievementsLayout = () => {
   return (
     <FullWidthContainer styles={""}>
       <div className={"h-screen w-full bg-[#E7E8E7] bg-cover bg-top xl:px-36 2xl:px-44 flex justify-center items-center"}>
-        <div className={"w-full h-full flex justify-center items-start"}>
+        <div className={"w-full h-full flex flex-col items-center"}>
           <h2 className={"mt-20 font-black uppercase xl:text-3xl 2xl:text-4xl strokeText"}>Our Achievements</h2>
+          <div className={"w-full h-full flex justify-between"}>
+            <div className={"w-[30%] h-full pt-28 flex flex-col justify-start items-start relative"}>
+              <div className={"w-full relative"}>
+              <div className={"font-black uppercase xl:text-9xl 2xl:text-9xl strokeText"}>
+                10
+              </div>
+              <h2 className={"absolute -bottom-8 left-10 uppercase font-medium text-main_dark xl:text-2xl 2xl:text-3xl"}>
+                years of work experience
+              </h2>
+              </div>
+            </div>
+            <div className={"w-[30%] h-full flex justify-center items-center"}>
+              <div className={"w-full relative"}>
+                <div className={"font-black uppercase xl:text-9xl 2xl:text-9xl strokeText"}>
+                  15
+                </div>
+                <h2 className={"absolute -bottom-8 left-10 uppercase font-medium text-main_dark xl:text-2xl 2xl:text-3xl"}>
+                  Countries we worked with
+                </h2>
+              </div>
+            </div>
+            <div className={"w-[30%] h-full pb-16 flex justify-end items-end"}>
+              <div className={"w-full relative"}>
+                <div className={"font-black uppercase xl:text-9xl 2xl:text-9xl strokeText"}>
+                  50+
+                </div>
+                <h2 className={"absolute bottom-0 left-10 uppercase font-medium text-main_dark xl:text-2xl 2xl:text-3xl"}>
+                  projects done
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </FullWidthContainer>
--- src/layouts/choose.layout.tsx
+++ src/layouts/choose.layout.tsx
@@ -4,8 +4,45 @@ export const ChooseLayout = () => {
   return (
     <FullWidthContainer styles={""}>
       <div className={"h-screen w-full bg-img_choose bg-cover bg-top xl:px-36 2xl:px-44 flex justify-center items-center"}>
-        <div className={"w-full h-full bg-rose-500/10 flex justify-center items-start"}>
+        <div className={"w-full h-full flex flex-col justify-center items-center"}>
           <h2 className={"mt-20 font-black uppercase xl:text-3xl 2xl:text-4xl strokeTextWhite"}>Why choose us</h2>
+          <div className={"w-full h-full py-14"}>
+            <div className={"w-full h-1/2 flex justify-evenly"}>
+              <div className={"w-1/4 h-full flex flex-col items-center justify-center"}>
+                <img src="/assets/choose/24-7.svg" alt=""/>
+                <h2 className={"xl:text-base 2xl:text-xl mt-5 text-white font-extralight text-center"}>
+                  We are in touch with you 24/7. And it`s OK if you are far from the technical knowledge and terms
+                </h2>
+              </div>
+              <div className={"w-1/4 h-full flex flex-col items-center justify-center"}>
+                <img src="/assets/choose/Full_Cycle.svg" alt=""/>
+                <h2 className={"xl:text-base 2xl:text-xl mt-5 text-white font-extralight text-center"}>
+                  We deal with the full development cycle. From the idea to the first client
+                </h2>
+              </div>
+              <div className={"w-1/4 h-full flex flex-col items-center justify-center"}>
+                <img src="/assets/choose/Agile.svg" alt=""/>
+                <h2 className={"xl:text-base 2xl:text-xl mt-5 text-white font-extralight text-center"}>
+                  We use Agile and SCRUM methods in our work
+                </h2>
+              </div>
+            </div>
+            <div className={"w-full h-1/2 flex justify-evenly pt-6"}>
+              <div className={"w-1/4 h-full flex flex-col items-center justify-center"}>
+                <img src="/assets/choose/NDA.svg" alt=""/>
+                <h2 className={"xl:text-base 2xl:text-xl mt-5 text-white font-extralight text-center"}>
+                  All ideas and commercial information will be protected.
+                  We always sign NDA
+                </h2>
+              </div>
+              <div className={"w-1/4 h-full flex flex-col items-center justify-center"}>
+                <img src="/assets/choose/100.svg" alt=""/>
+                <h2 className={"xl:text-base 2xl:text-xl mt-5 text-white font-extralight text-center"}>
+                  We are focused on results and want you to be 100% satisfied
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </FullWidthContainer>
